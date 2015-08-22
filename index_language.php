5c57678585e750f7540fea946b022a4f9c647b23
David Roesel <roesel@gmail.com>
2015-08-23 00:35:29 +0200

    Fix asense

--- index_language.php
+++ index_language.php
@@ -9,6 +9,16 @@
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Tangerine&amp;v1" />
   <link rel="stylesheet" type="text/css" href="http://fonts.googleapis.com/css?family=Yanone+Kaffeesatz" />
   <link rel="stylesheet" type="text/css" href="../style/style.css" />
+  <script type="text/javascript"> 
+	  var _gaq = _gaq || [];
+	  _gaq.push(['_setAccount', 'UA-18026034-1']);
+	  _gaq.push(['_trackPageview']);
+	  (function() {
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+	  })();
+  </script>
 </head>
 
 <body>
